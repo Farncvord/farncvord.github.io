e13c38c662bcd7d82021635c5a5e1ce839d21385
Aler II <sokrat@vip.dn.ua>
2024-06-15 18:47:12 +0300

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <template>
-  <div>
-    <button @click="increment(click_increment)">
-      {{money}}
-    </button>
-    <button @click="upgrade(click_increment)">
-      {{value(click_increment)}}
-    </button>
+  <div class="container py-5">
+    <div id="info" class="mb-5">
+      <div class="row w-50 me-auto ">
+        <div class="col align-self-center">
+          "Money":
+        </div>
+        <div class="col align-self-center">
+          {{ money }}
+        </div>
+      </div>
+      <div class="row w-50 me-auto ">
+        <div class="col align-self-center">
+          Power of "mining":
+        </div>
+        <div class="col align-self-center">
+          {{ value(click_increment) }}
+        </div>
+      </div>
+    </div>
+    <div id="controls" class="row">
+      <div class="col">
+        <button type="button" class="btn btn-outline-dark btn-lg w-100" @click="increment(click_increment)">
+          Mine ore... not yours. Get it?
+        </button>
+      </div>
+      <div class="col">
+        <button type="button" class="btn btn-outline-dark btn-lg w-100" @click="upgrade(click_increment)">
+          Upgrade your power (not actual power, just the number on top)
+        </button>
+        {{ price(click_increment) }} "money"
+      </div>
+    </div>
   </div>
 </template>
 
@@ -34,7 +59,7 @@ export default {
     },
 
     upgrade(param) {
-      let price = param.base_price + (param.level - 1) * param.increment_price;
+      let price = this.price(param);
 
       if( this.money >= price) {
         this.money -= price;
@@ -45,6 +70,10 @@ export default {
 
     value(param) {
       return param.base_value + (param.level - 1) * param.increment_value;
+    },
+
+    price(param) {
+      return param.base_price + (param.level - 1) * param.increment_price;
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import './assets/main.scss'
 import "bootstrap"
 
 import { createApp } from 'vue'
